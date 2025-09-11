0415d5df2810611a74392ffd881ac907e1dcd885
Muhammad Fareed <mfareedmughalinfo@gmail.com>
2025-09-11 15:14:24 +0500

    Refactor database connection logic and clean up unused code in db.js and index.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,24 +1,6 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
 
-// export let isConnected = false;
-
-// export const connectDb = async () => {
-//   try {
-//     const connnectionInstance = await mongoose.connect(
-//       `${process.env.MONGO_URI}/${DB_NAME}`
-//     );
-
-//     isConnected = true;
-//     console.log(
-//       `MONGODB connected || DB Host is ${connnectionInstance.connection.host}`
-//     );
-//   } catch (error) {
-//     console.log("DB error", error);
-//     process.exit(1);
-//   }
-// };
-
 let isConnected = false; // track connection
 
 export const connectDb = async () => {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { app } from "./app.js";
-import { connectDb, isConnected } from "./config/db.js";
+import { connectDb } from "./config/db.js";
 import dotenv from "dotenv";
 import { connectCloudinary } from "./utils/cdn.cloudinary.js";
 
@@ -8,22 +8,6 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 await connectCloudinary();
-// await connectDb()
-//   .then(() => {
-//     app.listen(PORT, () => {
-//       console.log(`Server is running at http://localhost:${PORT}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.log("connection failed...", error);
-//   });
-
-// app.use((req, res) => {
-//   if (!isConnected) {
-//     connectDb();
-//   }
-// });
-
 await connectDb(); // sirf cold start pe connect karega
 
 export default app;
